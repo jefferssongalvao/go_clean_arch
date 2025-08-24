7044db3bbd2e7bbeeda0b60a0f4ccbd1a53c98c2
jefferssongalvao <jefferssondeveloper@gmail.com>
2025-08-24 20:10:02 -0300

    configurações de senha

--- migrations/seeds/seed.go
+++ migrations/seeds/seed.go
@@ -1,7 +1,6 @@
 package seeds
 
 import (
-	"fmt"
 	"log"
 
 	valueobjects "github.com/jefferssongalvao/go_clean_arch/internal/domain/value_objects"
@@ -12,15 +11,8 @@ import (
 func Run() {
 	db := database.DB
 
-	// Lista de usuários e estudantes relacionados
 	pass1, _ := valueobjects.NewPassword("Bat@123")
-
-	fmt.Println("Password 1: ", pass1)
-
 	pass2, _ := valueobjects.NewPassword("super123")
-
-	fmt.Println("Password 2: ", pass2)
-
 	users := []models.User{
 		{Username: "bruce", Password: pass1.Hash()},
 		{Username: "clark", Password: pass2.Hash()},
@@ -32,11 +24,10 @@ func Run() {
 	}
 
 	for i, user := range users {
-		// Cria o usuário
 		if err := db.FirstOrCreate(&user, models.User{Username: user.Username}).Error; err != nil {
 			log.Fatalf("Erro ao criar usuário: %v", err)
 		}
-		// Associa o estudante ao usuário criado
+
 		students[i].UserID = user.ID
 		if err := db.FirstOrCreate(&students[i], models.Student{Name: students[i].Name}).Error; err != nil {
 			log.Fatalf("Erro ao criar estudante: %v", err)
